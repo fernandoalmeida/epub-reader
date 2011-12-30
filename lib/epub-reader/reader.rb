16d03a55fc675fa586b2be208e231244231ca012
Fernando Almeida <fernando@fernandoalmeida.net>
2011-12-29 21:01:08 -0300

    Get mime type and validates file (in progress)

--- lib/epub-reader/reader.rb
+++ lib/epub-reader/reader.rb
@@ -3,6 +3,8 @@ require 'zip/zipfilesystem'
 module Epub
   class Reader
 
+    EPUB_MIMETYPE = "application/epub+zip"
+
     attr_reader :filename, :file
     
     def initialize(file)
@@ -18,6 +20,18 @@ module Epub
       else
         reader
       end
+    end
+
+    def mimetype
+      begin
+        file.get_input_stream('mimetype').read
+      rescue
+        nil
+      end
+    end
+    
+    def valid?
+      mimetype == EPUB_MIMETYPE
     end
     
   end
--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -3,19 +3,33 @@ require 'spec_helper'
 describe Epub::Reader do
 
   before(:all) do
-    @epub = Epub::Reader.open('spec/data/valid.epub')
+    @file = 'spec/data/valid.epub'
+    @epub = Epub::Reader.open(@file)
   end
 
-  it 'should open a epub file' do
+  it 'open a epub file' do
     @epub.should_not be_nil
   end
 
-  it 'should raise exception if file not found' do
+  it 'should raise an exception if file not found' do
     lambda { Epub::Reader.open('not_found.epub') }.should raise_error
+  end
+
+  it 'informs the filename' do
+    @epub.filename.should eq(@file)
   end
 
   it 'should have the epub file to handle' do
     @epub.file.should_not be_nil
+  end
+
+  it 'informs the mime type' do
+    @epub.mimetype.should eq("application/epub+zip")
+  end
+
+  it 'validates the file' do
+    @epub.should be_valid
+    Epub::Reader.open('spec/data/invalid.epub').should_not be_valid
   end
 
 end
