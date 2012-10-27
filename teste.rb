99d1258c783b82422d906f20c90ea468deb65eb9
Fernando Almeida <fernando@fernandoalmeida.net>
2012-10-27 08:37:14 -0200

    Fix: get cover from malformed epubs

--- teste.rb
+++ teste.rb
@@ -1,9 +1,9 @@
-`rake install`
+# `rake install`
 require 'rubygems'
 require 'epub-reader'
 
-hd = '/home/fernando/trabalho/livros_e_apostilas/EPUB/hd.epub'
+hd = '/home/fernando/Dropbox/trabalho/livros_e_apostilas/EPUB/e_book_Marketing.epub'
 
 f1 = Epub::Reader.open(hd)
 
-puts f1.title
+puts f1.cover
